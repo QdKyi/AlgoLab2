58652734dc17ef1fecce4631362be8d423fea79b
QdKyi <denroy777@gmail.com>
2020-10-28 22:24:42 +0200

    Switched Insertion to QucikSort to improve algo speed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     calendar = MyList(
         [MyTuple(MyList([1, 2])), MyTuple(MyList([0, 3])), MyTuple(MyList([2, 4])),
-         MyTuple(MyList([6, 7])),MyTuple(MyList([7, 8])), MyTuple(MyList([9, 9]))])
+         MyTuple(MyList([6, 7])), MyTuple(MyList([7, 8])), MyTuple(MyList([9, 9]))])
     print(calendar)
     final_list = calendar_handler(calendar)
     print(final_list)
--- utils/sort_and_utils.py
+++ utils/sort_and_utils.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 from utils.my_list import MyList
 from utils.my_tuple import MyTuple
 
@@ -13,6 +15,48 @@ def insertion_sort(given_array):
     return given_array
 
 
+def partition(given_array, start, end):
+    pivot = given_array[start][0]
+    low = start + 1
+    high = end
+
+    while True:
+
+        while low <= high and given_array[high][0] > pivot:
+
+            high = high - 1
+
+        while low <= high and not given_array[low][0] > pivot:
+
+            low = low + 1
+
+        if low <= high:
+            swap(given_array, low, high)
+
+        else:
+            break
+    swap(given_array, start, high)
+
+    return high
+
+
+def quick_sort(given_array, start, end):
+    if start >= end:
+        return
+
+    my_partition = partition(given_array, start, end)
+
+    quick_sort(given_array, start, my_partition - 1)
+    quick_sort(given_array, my_partition + 1, end)
+
+
+def sort(given_array):
+
+    shuffle(given_array)
+
+    quick_sort(given_array, 0, len(given_array) - 1)
+
+
 def swap(given_array, first_elem, second_elem):
     given_array[first_elem], given_array[second_elem] = given_array[second_elem], given_array[first_elem]
 
@@ -23,7 +67,7 @@ def calendar_handler(calendar):
     >>> calendar_handler(calendar)
     [(0, 4), (6, 7)]
     """
-    insertion_sort(calendar)
+    sort(calendar)
 
     final_list = MyList()
 
